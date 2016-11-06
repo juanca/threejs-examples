d10c27c038a063d284728fc1e4617e5ebe1a6179
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-06 00:54:39 -0700

    They see me refactoring

--- multi-color-cube.js
+++ multi-color-cube.js
@@ -1,19 +1,17 @@
 var THREE = require('three');
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+scene.add( camera );
 
 var geometry = new THREE.BoxGeometry( 2, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { wireframe: true, color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-scene.add( camera );
-
-camera.position.z = 5;
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
 
 (function render() {
 	requestAnimationFrame( render );
