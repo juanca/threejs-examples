8c5c59f5f1a0ceb68a1ca92527b1183124268f86
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-06 00:59:02 -0700

    Gotta keep dat whiteline in place

--- multi-color-cube.js
+++ multi-color-cube.js
@@ -1,9 +1,9 @@
 var THREE = require('three');
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
-scene.add( camera );
+scene.add(camera);
 
 var geometry = new THREE.BoxGeometry( 2, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { wireframe: true, color: 0x00ff00 } );
@@ -11,18 +11,18 @@ var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 (function render() {
-	requestAnimationFrame( render );
+	requestAnimationFrame(render);
 
-  cube.translateX(0.001)
-  cube.rotateX(0)
-  cube.rotateY(0)
+  cube.translateX(0);
+  cube.rotateX(0.01);
+  cube.rotateY(0.02);
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 }())
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  document.body.appendChild( renderer.domElement );
+  document.body.appendChild(renderer.domElement);
 });
